cbcf5faff2404a2696138a629855e7f4b9a967d0
Ahmet Sinan Karakurt <ahmetsinankarakurt@gmail.com>
2023-05-26 09:30:16 +0000

    added qa automation pipeline for dev1.1

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -2,36 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 import os
 
-# Set chrome options for working with headless mode (no screen)
+# Chrome seçeneklerini başlatma (ekran olmadan çalışma)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 chrome_options.add_argument("no-sandbox")
 chrome_options.add_argument("disable-dev-shm-usage")
 
-# Update webdriver instance of chrome-driver with adding chrome options
+# Chrome-driver'ı chrome seçenekleriyle güncelleme
 driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome("/Users/home/Desktop/chromedriver")
-# Connect to the application
+
+# Uygulamaya bağlanma
 APP_IP = os.environ['MASTER_PUBLIC_IP']
 url = "http://"+APP_IP.strip()+":30001/"
-# url = "http://localhost:8080"
-print(url)
 driver.get(url)
-sleep(3)
-owners_link = driver.find_element_by_link_text("OWNERS")
+
+# Bekleme ekleyerek "OWNERS" bağlantısını bulma
+owners_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "OWNERS")))
 owners_link.click()
-sleep(2)
-all_link = driver.find_element_by_link_text("ALL")
+
+# Bekleme ekleyerek "ALL" bağlantısını bulma
+all_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "ALL")))
 all_link.click()
-sleep(2)
 
-# Verify that table loaded
-sleep(1)
+# Tablonun yüklendiğini doğrulama
 verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
-
 print("Table loaded")
 
 driver.quit()
